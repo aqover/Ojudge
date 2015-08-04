22df93834b43eb67c4de7235c4d6857088584350
Pongsathorn <bambi.elephant56@gmail.com>
2015-08-04 16:23:47 +0700

    Update index.php

--- index.php
+++ index.php
@@ -91,7 +91,7 @@ switch (ENVIRONMENT)
  * Include the path if the folder is not in the same directory
  * as this file.
  */
-	$system_path = '/home/aqover/system';
+	$system_path = 'system';
 
 /*
  *---------------------------------------------------------------
@@ -106,7 +106,7 @@ switch (ENVIRONMENT)
  *
  * NO TRAILING SLASH!
  */
-	$application_folder = '/home/aqover/application';
+	$application_folder = 'application';
 
 /*
  *---------------------------------------------------------------
